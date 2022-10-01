09ebd84fda27e057632fe0f600f86f9b10d0ec9b
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-10-01 22:45:49 +0200

    Top app bar refactoring

--- app/src/main/java/no/nordicsemi/android/wifi/provisioning/home/view/components/TopAppBar.kt
+++ app/src/main/java/no/nordicsemi/android/wifi/provisioning/home/view/components/TopAppBar.kt
@@ -31,17 +31,9 @@
 
 package no.nordicsemi.android.wifi.provisioning.home.view.components
 
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import no.nordicsemi.android.common.logger.view.LoggerAppBarIcon
 import no.nordicsemi.android.common.theme.view.NordicAppBar
-import no.nordicsemi.android.common.ui.logger.R as loggerR
 
 @Composable
 fun BackIconAppBar(text: String, onClick: () -> Unit) {
@@ -56,14 +48,7 @@ fun LoggerIconAppBar(text: String, onLoggerClick: () -> Unit) {
     NordicAppBar(
         text = text,
         actions = {
-            IconButton(onClick = { onLoggerClick() }) {
-                Icon(
-                    painterResource(id = loggerR.drawable.ic_logger),
-                    contentDescription = stringResource(id = loggerR.string.open_logger),
-                    tint = MaterialTheme.colorScheme.onPrimary,
-                    modifier = Modifier.size(24.dp)
-                )
-            }
+            LoggerAppBarIcon(onClick = { onLoggerClick() })
         }
     )
 }
