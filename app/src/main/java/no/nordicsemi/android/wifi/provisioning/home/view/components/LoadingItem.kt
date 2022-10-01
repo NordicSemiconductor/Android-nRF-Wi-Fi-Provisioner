015c63a40c4bbe1bdcd30e810b76b606eb5455dc
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-10-01 22:46:39 +0200

    Modified and preview

--- app/src/main/java/no/nordicsemi/android/wifi/provisioning/home/view/components/LoadingItem.kt
+++ app/src/main/java/no/nordicsemi/android/wifi/provisioning/home/view/components/LoadingItem.kt
@@ -16,11 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingItem(modifier: Modifier = Modifier) {
     Row(
+        modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Box(
@@ -63,4 +65,10 @@ fun LoadingItem(modifier: Modifier = Modifier) {
             Spacer(modifier = Modifier.size(2.dp))
         }
     }
+}
+
+@Preview
+@Composable
+fun LoadingItemPreview() {
+    LoadingItem()
 }
