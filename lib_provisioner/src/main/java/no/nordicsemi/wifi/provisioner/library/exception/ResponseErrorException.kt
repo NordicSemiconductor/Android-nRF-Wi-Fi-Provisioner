764151663686c6f8a32276ce4f148f5ed7c7f746
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-10-03 22:54:37 +0200

    Exception parsing

--- lib_provisioner/src/main/java/no/nordicsemi/wifi/provisioner/library/exception/ResponseErrorException.kt
+++ lib_provisioner/src/main/java/no/nordicsemi/wifi/provisioner/library/exception/ResponseErrorException.kt
@@ -1,7 +1,13 @@
 package no.nordicsemi.wifi.provisioner.library.exception
 
-data class ResponseErrorException(val code: ResponseError) : Exception("Received error response.")
+data class ResponseErrorException(val code: ResponseError) : Exception(code.toString())
 
 enum class ResponseError {
-    INVALID_ARGUMENT, INVALID_PROTO, INTERNAL_ERROR
+    INVALID_ARGUMENT, INVALID_PROTO, INTERNAL_ERROR;
+
+    override fun toString(): String = when (this) {
+        INVALID_ARGUMENT -> "Invalid argument"
+        INVALID_PROTO -> "Invalid proto"
+        INTERNAL_ERROR -> "Internal error"
+    }
 }
