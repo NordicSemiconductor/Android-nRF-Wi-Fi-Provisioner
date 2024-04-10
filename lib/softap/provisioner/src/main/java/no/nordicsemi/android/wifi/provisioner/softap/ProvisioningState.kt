7013a9e7d61a61eb650cbf3602e3d59088d11eee
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-04-10 14:14:09 +0200

    Improvements to ProvisioningState

--- lib/softap/provisioner/src/main/java/no/nordicsemi/android/wifi/provisioner/softap/ProvisioningState.kt
+++ lib/softap/provisioner/src/main/java/no/nordicsemi/android/wifi/provisioner/softap/ProvisioningState.kt
@@ -1,5 +1,7 @@
 package no.nordicsemi.android.wifi.provisioner.softap
 
+import android.net.nsd.NsdServiceInfo
+
 /**
  * Created by Roshan Rajaratnam on 23/02/2024.
  *
@@ -8,19 +10,29 @@ package no.nordicsemi.android.wifi.provisioner.softap
 sealed class ProvisioningState {
 
     /**
-     * State when connected to a Unprovisioned Wifi Node
+     * State when connected to a Unprovisioned Wifi Node.
      */
     data object Disconnected : ProvisioningState()
 
     /**
-     * State when connected to a Unprovisioned Wifi Node
+     * State when connected to a Unprovisioned Wifi Node.
      */
     data object Connecting : ProvisioningState()
 
     /**
-     * State when connected to a Unprovisioned Wifi Node
+     * State when connected to a Unprovisioned Wifi Node.
      */
     data object Connected : ProvisioningState()
+
+    /**
+     * State when the discovering the network services available on the network.
+     */
+    data object InitiatingNetworkServiceDiscovery : ProvisioningState()
+
+    /**
+     * State when the network service discovery is complete.
+     */
+    data class NetworkServiceDiscoveryComplete(val serviceInfo : NsdServiceInfo) : ProvisioningState()
 
     /**
      * State when connected to a Unprovisioned Wifi Node
