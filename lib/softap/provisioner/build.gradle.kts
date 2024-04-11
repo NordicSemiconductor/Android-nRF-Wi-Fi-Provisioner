d57b9d2420c821902653f65b9b2fafb8b103a98a
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-04-11 12:48:16 +0200

    Add missing wire converter factory

--- lib/softap/provisioner/build.gradle.kts
+++ lib/softap/provisioner/build.gradle.kts
@@ -10,8 +10,10 @@ android {
 dependencies {
     implementation(project(":lib:softap:proto"))
     implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
     implementation(libs.retrofit.converter.scalars)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation("com.squareup.retrofit2:converter-wire:2.11.0")
     implementation("com.squareup.okhttp3:okhttp-tls:5.0.0-alpha.12")
 }
