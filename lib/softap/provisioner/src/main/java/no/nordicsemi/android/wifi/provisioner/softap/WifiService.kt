e232d4ea2c063f79214bd3c5015736387f8af547
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-04-11 11:03:31 +0200

    Clean up WifiService API

--- lib/softap/provisioner/src/main/java/no/nordicsemi/android/wifi/provisioner/softap/WifiService.kt
+++ lib/softap/provisioner/src/main/java/no/nordicsemi/android/wifi/provisioner/softap/WifiService.kt
@@ -1,12 +1,17 @@
 package no.nordicsemi.android.wifi.provisioner.softap
 
+import no.nordicsemi.android.wifi.provisioner.softap.proto.ScanResults
+import no.nordicsemi.android.wifi.provisioner.softap.proto.WifiConfig
+import no.nordicsemi.android.wifi.provisioner.softap.proto.WifiScanResult
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
+import java.net.InetAddress
 
 /**
  * Created by Roshan Rajaratnam on 19/02/2024.
@@ -15,13 +20,9 @@ interface WifiService {
 
     @Headers("Content-Type: text/plain")
     @GET("prov/networks")
-    suspend fun listSsids(): Response<ResponseBody>
+    suspend fun listSsids(): ScanResults
 
     @Headers("Content-Type: text/plain")
-    @PUT("prov/configure")
-    suspend fun provision(@Body credentials: String): Response<ResponseBody>
-
-    @Headers("Content-Type: text/plain")
-    @PUT("led/{led}")
-    suspend fun blink(@Path("led") led : Int, @Body toggle: String): Response<ResponseBody>
+    @POST("prov/configure")
+    suspend fun provision(@Body config: WifiConfig): Response<ResponseBody>
 }
