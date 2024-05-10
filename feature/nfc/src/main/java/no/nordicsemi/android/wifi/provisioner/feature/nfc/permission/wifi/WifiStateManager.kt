135feaf8c9024d99b4c6eadda9d72fc743bf3484
hiar <aryal.himali@gmail.com>
2024-05-10 16:17:55 +0200

    wifi permission state

---
+++ feature/nfc/src/main/java/no/nordicsemi/android/wifi/provisioner/feature/nfc/permission/wifi/WifiStateManager.kt
@@ -0,0 +1,85 @@
+package no.nordicsemi.android.wifi.provisioner.feature.nfc.permission.wifi
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.RECEIVER_EXPORTED
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import no.nordicsemi.android.wifi.provisioner.feature.nfc.permission.utils.LocalDataProvider
+import no.nordicsemi.android.wifi.provisioner.feature.nfc.permission.utils.PermissionUtils
+import no.nordicsemi.android.wifi.provisioner.feature.nfc.permission.utils.WifiPermissionNotAvailableReason
+import no.nordicsemi.android.wifi.provisioner.feature.nfc.permission.utils.WifiPermissionState
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private const val REFRESH_PERMISSIONS =
+    "no.nordicsemi.android.common.permission.REFRESH_WIFI_PERMISSIONS"
+
+@Singleton
+class WifiStateManager @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+    private val dataProvider = LocalDataProvider(context)
+    private val utils = PermissionUtils(context, dataProvider)
+
+    @SuppressLint("WrongConstant")
+    fun wifiState() = callbackFlow {
+        trySend(getBluetoothPermissionState())
+
+        val wifiStateChangeHandler = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                trySend(getBluetoothPermissionState())
+            }
+        }
+        val filter = IntentFilter().apply {
+            addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
+            addAction(REFRESH_PERMISSIONS)
+        }
+
+        ContextCompat.registerReceiver(
+            context,
+            wifiStateChangeHandler,
+            filter,
+            RECEIVER_EXPORTED
+        )
+
+        awaitClose {
+            context.unregisterReceiver(wifiStateChangeHandler)
+        }
+    }
+
+    fun refreshPermission() {
+        val intent = Intent(REFRESH_PERMISSIONS)
+        context.sendBroadcast(intent)
+    }
+
+    fun markWifiPermissionRequested() {
+        dataProvider.wifiPermissionRequested = true
+    }
+
+    fun isWifiPermissionDeniedForever(context: Context): Boolean {
+        return utils.isWifiPermissionDeniedForever(context)
+    }
+
+    private fun getBluetoothPermissionState() = when {
+        !utils.isWifiAvailable -> WifiPermissionState.NotAvailable(
+            WifiPermissionNotAvailableReason.NOT_AVAILABLE
+        )
+
+        !utils.areNecessaryWifiPermissionsGranted -> WifiPermissionState.NotAvailable(
+            WifiPermissionNotAvailableReason.PERMISSION_REQUIRED
+        )
+
+        !utils.isWifiEnabled -> WifiPermissionState.NotAvailable(
+            WifiPermissionNotAvailableReason.DISABLED
+        )
+
+        else -> WifiPermissionState.Available
+    }
+}
