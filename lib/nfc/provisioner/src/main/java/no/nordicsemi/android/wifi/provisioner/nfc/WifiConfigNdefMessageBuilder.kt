c134110406386d449e3bccdde9eb207eaf771117
hiar <aryal.himali@gmail.com>
2024-05-22 12:09:43 +0200

    Updated Ndef message builder

--- lib/nfc/provisioner/src/main/java/no/nordicsemi/android/wifi/provisioner/nfc/WifiConfigNdefMessageBuilder.kt
+++ lib/nfc/provisioner/src/main/java/no/nordicsemi/android/wifi/provisioner/nfc/WifiConfigNdefMessageBuilder.kt
@@ -2,68 +2,24 @@ package no.nordicsemi.android.wifi.provisioner.nfc
 
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
-import android.provider.ContactsContract
 import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiData
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.AUTH_TYPE_FIELD_ID
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.AUTH_TYPE_WPA2_PSK
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.CREDENTIAL_FIELD_ID
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.ENC_TYPE_AES
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.ENC_TYPE_FIELD_ID
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.MAX_MAC_ADDRESS_SIZE_BYTES
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.NETWORK_KEY_FIELD_ID
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.NFC_TOKEN_MIME_TYPE
+import no.nordicsemi.android.wifi.provisioner.nfc.domain.WifiHandoverDataType.SSID_FIELD_ID
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 import javax.inject.Inject
-import kotlin.math.floor
-
-internal const val NFC_TOKEN_MIME_TYPE: String = "application/vnd.wfa.wsc"
-private val CREDENTIAL_FIELD_ID = byteArrayOf(0x10, 0x0e)
-private val NETWORK_IDX: ByteArray = byteArrayOf(0x10, 0x26)
-private val WPS_AUTH_WPA2_PERSONAL: ByteArray = byteArrayOf(0x00, 0x20)
-
-private val WPS_CRYPT_AES: ByteArray = byteArrayOf(0x00, 0x08)
-private val AUTH_TYPE: ByteArray = byteArrayOf(0x10, 0x03)
-private val AUTH_WPA_PERSONAL: ByteArray = byteArrayOf(0x00, 0x02)
-private val NETWORK_KEY: ByteArray = byteArrayOf(0x10, 0x27)
-private val NETWORK_NAME: ByteArray = byteArrayOf(0x10, 0x45)
-
-private val CRYPT_TYPE: ByteArray = byteArrayOf(0x10, 0x0F)
-private val CRYPT_WEP: ByteArray = byteArrayOf(0x00, 0x02)
 
 /**
  * This class is responsible for creating the NDEF message for the WiFi data.
  */
 class WifiConfigNdefMessageBuilder @Inject constructor() {
-
-    /**
-     * Creates the NDEF record for the WiFi data.
-     *
-     * @param wifiData the WiFi data to be written to the NDEF record.
-     * @return the NDEF record for the WiFi data.
-     */
-    private fun createWifiRecord(wifiData: WifiData): NdefRecord {
-        val ssid = wifiData.ssid
-        val password = wifiData.password
-        val auth = wifiData.authType // TODO: For now, we are only supporting WPA2_PSK.
-        val crypt = "AES" // TODO: For now, we are only supporting AES.
-        val authByte = WPS_AUTH_WPA2_PERSONAL
-        val cryptByte = WPS_CRYPT_AES
-        val ssidByte = ssid.toByteArray()
-        val passwordByte = password.toByteArray()
-        val ssidLength = byteArrayOf(
-            floor((ssid.length / 256).toDouble())
-                .toInt().toByte(), (ssid.length % 256).toByte()
-        )
-        val passwordLength = byteArrayOf(
-            floor((password.length / 256).toDouble())
-                .toInt().toByte(), (password.length % 256).toByte()
-        )
-        val cred = byteArrayOf(0x00, 0x36)
-        val idx = byteArrayOf(0x00, 0x01, 0x01)
-        val mac = byteArrayOf(0x00, 0x06) // TODO: Get the mac address of the device.
-        val keypad = byteArrayOf(0x00, 0x0B) // TODO: Get the keypad of the device.
-
-        val payload: ByteArray = concat(
-            CREDENTIAL_FIELD_ID, cred,
-            NETWORK_IDX, idx,
-            NETWORK_NAME, ssidLength, ssidByte,
-            AUTH_TYPE, AUTH_WPA_PERSONAL, authByte,
-            CRYPT_TYPE, CRYPT_WEP, cryptByte,
-            NETWORK_KEY, passwordLength, passwordByte
-        )
-        return NdefRecord.createMime(NFC_TOKEN_MIME_TYPE, payload)
-    }
 
     /**
      * Creates the NDEF message for the WiFi data.
@@ -72,43 +28,74 @@ class WifiConfigNdefMessageBuilder @Inject constructor() {
      * @return the NDEF message for the WiFi data.
      */
     fun createNdefMessage(wifiNetwork: WifiData): NdefMessage {
-        val version = byteArrayOf(((0x1 shl 4) or (0x2)).toByte())
-        // TODO: Not sure is this is needed.
-        val handOverRecord = NdefRecord(
-            NdefRecord.TNF_WELL_KNOWN,
-            NdefRecord.RTD_HANDOVER_REQUEST,
-            ByteArray(0),
-            version
-        )
-        // TODO: Not sure is this is needed.
-        val aar = NdefRecord.createApplicationRecord(ContactsContract.Directory.PACKAGE_NAME)
-        val wifiRecord = createWifiRecord(wifiNetwork)
-        return NdefMessage(arrayOf(wifiRecord)) //, handOverRecord, aar))
+        return generateNdefMessage(wifiNetwork)
     }
 
     /**
-     * Concatenates the given byte arrays into a single byte array.
+     * Generates the NDEF message for the given WiFi network.
      *
-     * @param arrays the byte arrays to be concatenated.
-     * @return the concatenated byte array.
+     * @param wifiNetwork the WiFi network to be written to the NDEF message.
      */
-    private fun concat(vararg arrays: ByteArray): ByteArray {
-        // Calculate the total length of the resulting ByteArray
-        val totalLength = arrays.sumOf { it.size }
+    private fun generateNdefMessage(wifiNetwork: WifiData): NdefMessage {
+        val payload: ByteArray = generateNdefPayload(wifiNetwork)
+        val empty = byteArrayOf()
 
-        // Create a new ByteArray with the calculated length
-        val result = ByteArray(totalLength)
+        val mimeRecord = NdefRecord(
+            NdefRecord.TNF_MIME_MEDIA,
+            NFC_TOKEN_MIME_TYPE.toByteArray(Charset.forName("US-ASCII")),
+            empty,
+            payload
+        )
 
-        // Keep track of the current position in the result array
-        var currentPos = 0
+        return NdefMessage(arrayOf(mimeRecord))
+    }
 
-        // Copy each ByteArray into the result array
-        for (array in arrays) {
-            System.arraycopy(array, 0, result, currentPos, array.size)
-            currentPos += array.size
+    /**
+     * Generates the NDEF payload for the given WiFi network.
+     *
+     * @param wifiNetwork the WiFi network to be written to the NDEF message.
+     */
+    private fun generateNdefPayload(wifiNetwork: WifiData): ByteArray {
+        val ssid: String = wifiNetwork.ssid
+        val ssidSize = ssid.toByteArray().size.toShort()
+        val authType: Short = AUTH_TYPE_WPA2_PSK
+        val networkKey: String = wifiNetwork.password
+        val networkKeySize = networkKey.toByteArray().size.toShort()
+
+        val macAddress = ByteArray(MAX_MAC_ADDRESS_SIZE_BYTES)
+        for (i in 0 until MAX_MAC_ADDRESS_SIZE_BYTES) {
+            macAddress[i] = 0xff.toByte()
         }
 
-        return result
+        /* Fill buffer */
+        val bufferSize = 24 + ssidSize + networkKeySize // size of required credential attributes
+
+        // Create a buffer with the required size
+        val buffer = ByteBuffer.allocate(bufferSize)
+        buffer.putShort(CREDENTIAL_FIELD_ID)
+        buffer.putShort((bufferSize - 4).toShort())
+
+        // Add the SSID
+        buffer.putShort(SSID_FIELD_ID)
+        buffer.putShort(ssidSize)
+        buffer.put(ssid.toByteArray())
+
+        // Add authentication type
+        buffer.putShort(AUTH_TYPE_FIELD_ID)
+        buffer.putShort(2.toShort())
+        buffer.putShort(authType)
+
+        // Add encryption type
+        buffer.putShort(ENC_TYPE_FIELD_ID)
+        buffer.putShort(2.toShort())
+        buffer.putShort(ENC_TYPE_AES)
+
+        // Add network key / password
+        buffer.putShort(NETWORK_KEY_FIELD_ID)
+        buffer.putShort(networkKeySize)
+        buffer.put(networkKey.toByteArray())
+
+        return buffer.array()
     }
 
 }
