18408da9db6cedcd82a77367d2c6245d956b8bf3
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-06-04 15:24:18 +0200

    Fixes the home screen with correct padding

--- feature/ui/src/main/java/no/nordicsemi/android/wifi/provisioner/ui/view/section/ProvisionSection.kt
+++ feature/ui/src/main/java/no/nordicsemi/android/wifi/provisioner/ui/view/section/ProvisionSection.kt
@@ -3,7 +3,6 @@ package no.nordicsemi.android.wifi.provisioner.ui.view.section
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
