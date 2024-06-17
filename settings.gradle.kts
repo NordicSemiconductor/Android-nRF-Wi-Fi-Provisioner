f2a0e279d0f47d3c55a4302554f069ede2e25642
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2024-06-17 21:32:49 +0200

    Version catalog updated to 1.11.12

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -48,7 +48,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.11.10")
+            from("no.nordicsemi.android.gradle:version-catalog:1.11.12")
         }
     }
 }
